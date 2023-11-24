0f7932b120e2f02ed0647177aef7e99cee662c87
quemet <129742514+quemet@users.noreply.github.com>
2023-11-24 12:16:25 +0100

    Delete ScriptPowershell directory

--- ScriptPowershell/Exercice1/4-Create-User.ps1
+++
@@ -1,9 +0,0 @@
-<#
-ETML
-Auteur : Quentin Métroz
-Date : 22.11.2023
-Description : Crée un nouvelle utilisateur en local avec son nom et son mot de passe passé en paramètres
-#>
-Import-Module microsoft.powershell.localaccounts -UseWindowsPowerShell;
-$s= $args[1] | ConvertTo-SecureString -AsPlainText -Force;
-New-LocalUser -Name $args[0] -Password $s;
--- ScriptPowershell/Exercice1/5-Add-UserGroup.ps1
+++
@@ -1,7 +0,0 @@
-<#
-ETML
-Auteur : Quentin Métroz
-Date : 22.11.2023
-Description : Ce Script ajoute à un groupe local un utilisateur, le nom de l'utilisateur et le nom du groupe sont passé en paramètres
-#>
-Add-LocalGroupMember -Group $args[1] -Member $args[0];
